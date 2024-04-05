74d2876646878f39f478c9fd78e6a90c08fae6c8
Nickshay27 <nickshaychouhan27@gmail.com>
2024-04-05 10:48:55 +0530

    deleted

--- Password_Manager/src/Componets/Manager.jsx
+++ Password_Manager/src/Componets/Manager.jsx
@@ -10,17 +10,22 @@ const Manager = () => {
   const [form, setForm] = useState({ site: "", username: "", password: "" });
   const [passwordArray, setPasswordArray] = useState([]);
 
+  const getPasswords = async () => {
+    let req = await fetch("http://localhost:4003/");
+    let passwords = await req.json();
+
+    console.log(passwords);
+    setPasswordArray(passwords);
+  };
+
   useEffect(() => {
-    let passwords = localStorage.getItem("passwords");
-    if (passwords) {
-      setPasswordArray(JSON.parse(passwords));
-    }
+    getPasswords();
   }, []);
 
   const copyText = (text) => {
     toast("📄 Copy to Clickboard Succesfull", {
       position: "top-center",
-      autoClose: 2000,
+      autoClose: 1500,
       hideProgressBar: false,
       closeOnClick: true,
       pauseOnHover: true,
@@ -37,17 +42,65 @@ const Manager = () => {
     const hiddenIcon = "/images/hidden.png";
     ref.current.src = showPassword ? hiddenIcon : eyeIcon;
   };
-  const savePassword = () => {
-    const newPasswordArray = [...passwordArray, { ...form, id: uuidv4() }];
-    setPasswordArray(newPasswordArray);
-    localStorage.setItem(
-      "passwords",
-      JSON.stringify(newPasswordArray, { ...form, id: uuidv4() })
-    );
-    setForm({ site: "", username: "", password: "" }); // Clear the form after saving
-    console.log(newPasswordArray);
+  const savePassword = async () => {
+    // Destructure form values
+    const { site, username, password } = form;
+
+    // Check if all fields have length greater than 3
+    if (site.length > 3 && username.length > 3 && password.length > 3) {
+      //If any such id exists in the db deleted
+      await fetch("http://localhost:4003/", {
+        method: "DELETE",
+        headers: { "content-Type": "application/json" },
+        body: JSON.stringify({ id: form.id }),
+      });
+
+      // Create a new password object with unique ID
+      const newPassword = { site, username, password, id: uuidv4() };
+
+      // Update passwordArray with the new password
+      const newPasswordArray = [...passwordArray, newPassword];
+      setPasswordArray(newPasswordArray);
+      await fetch("http://localhost:4003/", {
+        method: "POST",
+        headers: { "content-Type": "application/json" },
+        body: JSON.stringify({ ...form, id: uuidv4() }),
+      });
+
+      // Store passwordArray in localStorage
+      //      localStorage.setItem("passwords", JSON.stringify(newPasswordArray));
+
+      // Clear the form after saving
+      setForm({ site: "", username: "", password: "" });
+
+      // Display success message
+      toast("✔️ Password saved!", {
+        position: "top-center",
+        autoClose: 1500,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+    } else {
+      // Display error message if any field is invalid
+      toast("Error: Password not saved! ");
+    }
   };
-  const deletePassword = (id) => {
+
+  const deletePassword = async (id) => {
+    toast("🗑️ Password Deleted Succesfull", {
+      position: "top-center",
+      autoClose: 1500,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
     const confirmation = window.confirm(
       "Are you sure you want to delete this password?"
     );
@@ -55,7 +108,12 @@ const Manager = () => {
       console.log("Deleting Password With id", id);
       const updatedPasswords = passwordArray.filter((item) => item.id !== id);
       setPasswordArray(updatedPasswords);
-      localStorage.setItem("passwords", JSON.stringify(updatedPasswords));
+      let res = await fetch("http://localhost:4003/", {
+        method: "DELETE",
+        headers: { "content-Type": "application/json" },
+        body: JSON.stringify({ id }),
+      });
+      //  localStorage.setItem("passwords", JSON.stringify(updatedPasswords));
     } else {
       console.log("Deletion canceled by user.");
     }
@@ -63,13 +121,8 @@ const Manager = () => {
 
   const editPassword = (id) => {
     console.log("Editing Password With id", id);
-    setForm(passwordArray.filter((i) => i.id === id)[0]);
+    setForm({ ...passwordArray.filter((i) => i.id === id)[0], id: id });
     setPasswordArray(passwordArray.filter((item) => item.id !== id));
-
-    localStorage.setItem(
-      "passwords",
-      JSON.stringify(passwordArray.filter((item) => item.id !== id))
-    );
   };
 
   const handleChange = (e) => {
@@ -96,7 +149,7 @@ const Manager = () => {
       />
       ;
       <div className="absolute top-0 z-[-2] h-screen w-screen rotate-180 transform bg-green-50 bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)]"></div>
-      <div className="mycontainer">
+      <div className="p-3 md:p-0 md:mycontainer min-h-[88.2vh]">
         <h1 className="text-4xl font-bold text-center">
           <span className="text-green-500">&lt;</span>
           <span>Pass</span>
@@ -113,6 +166,7 @@ const Manager = () => {
             className="w-full p-4 py-1 rounded-full border border-green-500"
             type="text"
             name="site"
+            id="site"
           />
           <div className="flex w-full justify-between gap-8">
             <input
@@ -122,15 +176,17 @@ const Manager = () => {
               className="w-full p-4 py-1 rounded-full border border-green-500"
               type="text"
               name="username"
+              id="username"
             />
             <div className="relative">
               <input
                 onChange={handleChange}
                 value={form.password}
-                placeholder="Enter Password"
-                className="w-full p-4 py-1 rounded-full border border-green-500"
+                placeholder="Password"
+                className="edge-hide-password-eye w-full  p-4 py-1 rounded-full border border-green-500"
                 type={showPassword ? "text" : "password"}
                 name="password"
+                id="password"
               />
               <span className="absolute right-0 top-1">
                 <img
@@ -159,7 +215,7 @@ const Manager = () => {
           <h2 className="font-bold text-2xl py-4">Your Passwords</h2>
           {passwordArray.length === 0 && <div>No Passwords to show</div>}
           {passwordArray.length != 0 && (
-            <table className="table-auto w-full rounded-md  overflow-hidden">
+            <table className="table-auto w-full rounded-md  overflow-hidden mb-10">
               <thead className=" bg-green-800 text-white">
                 <tr>
                   <th className="py-2 ">Website/URL</th>
@@ -221,7 +277,7 @@ const Manager = () => {
 
                       <td className="  py-2 border border-white  text-centre ">
                         <div className=" flex items-center justify-center">
-                          <span> {item.password}</span>
+                          <span> {"*".repeat(item.password.length)}</span>
                           <div
                             className=" lordiconcopy size-7 cursor-pointer"
                             onClick={() => {
